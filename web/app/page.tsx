fc10b909cdc1e717bb07f05e7661c06a4b4bfa43
AliAlsannaa <ans85122@gmail.com>
2024-11-07 20:20:39 +0300

    charts

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -11,12 +11,24 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  Bar, 
+  BarChart,
+  Rectangle,
 } from "recharts";
 import {
-  ChartContainer,
-  ChartTooltipContent,
-  ChartLegendContent,
-} from "../components/ui/chart";
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 import {
   Table,
   TableHeader,
@@ -43,7 +55,7 @@ import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { DataTable } from "../components/ui/data-table";
 import { FaUser, FaUsers, FaClipboardList } from 'react-icons/fa';
-import { MoreHorizontal, ArrowUpDown } from "lucide-react"
+import { MoreHorizontal, ArrowUpDown, TrendingUp } from "lucide-react"
 import { Checkbox } from "@/components/ui/checkbox"
 
 // types just for experiment
@@ -98,17 +110,65 @@ const MainDashboard = () => {
         { id: "18345", title: "Forgot password", category: "Security", date: "2024/12/06" },
     ];
 
-  const messagesInGroupsData = [
-    { day: "2024-11-01", messages: 30 },
-    { day: "2024-11-02", messages: 45 },
-    { day: "2024-11-03", messages: 50 },
+    const categoryCounts = reports.reduce((acc, report) => {
+        const category = report.category;
+        if (acc[category]) {
+            acc[category] += 1;
+        } else {
+            acc[category] = 1;
+        }
+        return acc;
+    }, {});
+
+    const reportChartData = Object.entries(categoryCounts).map(([category, count]) => ({
+        category: category,
+        reports: count,
+    }));
+
+    const uniqueCategories = [...new Set(reports.map(report => report.category))];
+
+    const reportChartConfig = {
+        visitors: {
+            label: "Visitors",
+        },
+        ...uniqueCategories.reduce((config, category, index) => {
+            config[category] = {
+                label: category,
+                color: `hsl(var(--chart-${index + 1}))`
+            };
+            return config;
+        }, {})
+    } satisfies ChartConfig
+
+
+  const messages = [
+    { date: "2024-11-01", groups: 30, ai: 10 },
+    { date: "2024-11-02", groups: 45, ai: 20 },
+    { date: "2024-11-03", groups: 50, ai: 15 },
+    { date: "2024-11-04", groups: 60, ai: 18 },
+    { date: "2024-11-05", groups: 35, ai: 20 },
+    { date: "2024-11-06", groups: 50, ai: 25 },
+    { date: "2024-11-07", groups: 80, ai: 40 },
+    { date: "2024-11-08", groups: 55, ai: 30 },
+    { date: "2024-11-09", groups: 60, ai: 25 },
+    { date: "2024-11-10", groups: 60, ai: 48 },
+    { date: "2024-11-11", groups: 35, ai: 20 },
+    { date: "2024-11-12", groups: 50, ai: 25 },
   ];
 
-  const messagesInAIData = [
-    { day: "2024-11-01", messages: 10 },
-    { day: "2024-11-02", messages: 20 },
-    { day: "2024-11-03", messages: 15 },
-    ];
+    const chartConfig = {
+        views: {
+            label: "Messgaes",
+        },
+        groups: {
+            label: "Groups",
+            color: "hsl(var(--chart-1))",
+        },
+        ai: {
+            label: "AI",
+            color: "hsl(var(--chart-2))",
+        },
+    } satisfies ChartConfig
 
     const columns: ColumnDef<Users>[] = [
         {
@@ -344,6 +404,7 @@ const MainDashboard = () => {
         },
     ]
 
+
   const renderTableContent = (contentType) => {
     if (contentType === "users") {
       return (
@@ -402,25 +463,42 @@ const MainDashboard = () => {
         </ResponsiveContainer>
       </ChartContainer>
     );
-  };
+    };
+
+    const [isTableView, setIsTableView] = useState(true);
+
+    const [activeChart, setActiveChart] =
+        React.useState<keyof typeof chartConfig>("groups")
+    const total = React.useMemo(
+        () => ({
+            groups: messages.reduce((acc, curr) => acc + curr.groups, 0),
+            ai: messages.reduce((acc, curr) => acc + curr.ai, 0),
+        }),
+        []
+    )
+
+
+    const toggleView = () => {
+        setIsTableView(!isTableView);
+    };
 
   return (
-      <Tabs defaultValue="users" className="mt-12 mx-2  flex-col">
-          <TabsList className=" flex-col">
-              <TabsTrigger className=" flex-col px-6" value="users">
+      <Tabs defaultValue="users" className="mt-12  flex-col">
+          <TabsList className=" flex-col fixed py-8 px-2">
+              <TabsTrigger className=" flex-col px-4 rounded-2xl" value="users">
                       <FaUser />
                       <div className="text-sm">Users</div>
               </TabsTrigger>
-              <TabsTrigger className=" flex-col px-6" value="groups">
+              <TabsTrigger className=" flex-col px-4 rounded-2xl" value="groups">
                   <FaUsers />
                   <div className="text-sm">Groups</div>
               </TabsTrigger>
-              <TabsTrigger className=" flex-col px-6" value="reports">
+              <TabsTrigger className=" flex-col px-4 rounded-2xl" value="reports">
                   <FaClipboardList />
                   <div className="text-sm">Reports</div>
               </TabsTrigger>
           </TabsList>
-          <TabsContent value="users">
+          <TabsContent value="users" className="p-3">
           <div className="pl-28">
                   <div className="text-2xl">Table of users</div>
                   <div className="container mx-auto py-10">
@@ -428,19 +506,173 @@ const MainDashboard = () => {
                   </div>
           </div>
           </TabsContent>
-          <TabsContent value="groups">
+          <TabsContent value="groups" className="p-3">
+          <div>
+              {isTableView ? (
               <div className="pl-28">
                   <div className="text-2xl">Table of groups</div>
                   <div className="container mx-auto py-10">
                       <DataTable columns={columnsGroups} data={groups} placeholder="Filter course name.." tcolumn="name" />
                   </div>
+              </div>): (
+                  <div className="pl-28">
+                    <div className="text-2xl">Chart of messgaes</div>
+                    <div className="container mx-auto py-10">
+                              <Card>
+                                  <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+                                      <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+                                          <CardTitle>Messages Bar Chart</CardTitle>
+                                          <CardDescription>
+                                              Showing number of daily messages 
+                                          </CardDescription>
+                                      </div>
+                                      <div className="flex">
+                                          {["groups", "ai"].map((key) => {
+                                              const chart = key as keyof typeof chartConfig
+                                              return (
+                                                  <button
+                                                      key={chart}
+                                                      data-active={activeChart === chart}
+                                                      className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
+                                                      onClick={() => setActiveChart(chart)}
+                                                  >
+                                                      <span className="text-xs text-muted-foreground">
+                                                          {chartConfig[chart].label}
+                                                      </span>
+                                                      <span className="text-lg font-bold leading-none sm:text-3xl">
+                                                          {total[key as keyof typeof total].toLocaleString()}
+                                                      </span>
+                                                  </button>
+                                              )
+                                          })}
+                                      </div>
+                                  </CardHeader>
+                                  <CardContent className="px-2 sm:p-6">
+                                      <ChartContainer
+                                          config={chartConfig}
+                                          className="aspect-auto h-[250px] w-full"
+                                      >
+                                          <BarChart
+                                              accessibilityLayer
+                                              data={messages}
+                                              margin={{
+                                                  left: 12,
+                                                  right: 12,
+                                              }}
+                                          >
+                                              <CartesianGrid vertical={false} />
+                                              <XAxis
+                                                  dataKey="date"
+                                                  tickLine={false}
+                                                  axisLine={false}
+                                                  tickMargin={8}
+                                                  minTickGap={32}
+                                                  tickFormatter={(value) => {
+                                                      const date = new Date(value)
+                                                      return date.toLocaleDateString("en-US", {
+                                                          month: "short",
+                                                          day: "numeric",
+                                                      })
+                                                  }}
+                                              />
+                                              <ChartTooltip
+                                                  content={
+                                                      <ChartTooltipContent
+                                                          className="w-[150px]"
+                                                          nameKey="views"
+                                                          labelFormatter={(value) => {
+                                                              return new Date(value).toLocaleDateString("en-US", {
+                                                                  month: "short",
+                                                                  day: "numeric",
+                                                                  year: "numeric",
+                                                              })
+                                                          }}
+                                                      />
+                                                  }
+                                              />
+                                              <Bar dataKey={activeChart} fill={`var(--color-${activeChart})`} />
+                                          </BarChart>
+                                      </ChartContainer>
+                                  </CardContent>
+                              </Card>
+                    </div>
+                  </div>)
+              }
+              <div className="z-30">
+              <button
+                  onClick={toggleView}
+                  className="fixed bottom-8 right-8 px-4 py-2 rounded-full font-semibold bg-selected text-white shadow-lg hover:bg-primary-dark/90 focus:outline-none"
+              >
+                  {isTableView ? 'Switch to Chart' : 'Switch to Table'}
+              </button>
               </div>
+           </div>
           </TabsContent>
-          <TabsContent value="reports">
-              <div className="pl-28">
-                  <div className="text-2xl">Table of reports</div>
-                  <div className="container mx-auto py-10">
-                      <DataTable columns={columnsReports} data={reports} placeholder="Filter category.." tcolumn="category" />
+          <TabsContent value="reports" className="p-3">
+              <div>
+                  {isTableView ? (
+                      <div className="pl-28">
+                          <div className="text-2xl">Table of reports</div>
+                          <div className="container mx-auto py-10">
+                              <DataTable columns={columnsReports} data={reports} placeholder="Filter category.." tcolumn="category" />
+                          </div>
+                      </div>) : (
+                      <div className="pl-28">
+                          <div className="text-2xl">Chart of reports</div>
+                          <div className="container mx-auto py-10">
+                                  <Card>
+                                      <CardHeader>
+                                          <CardTitle>Reports of each category</CardTitle>
+                                          <CardDescription>Amount of reports submitted to each categiry</CardDescription>
+                                      </CardHeader>
+                                      <CardContent>
+                                          <ChartContainer config={reportChartConfig}>
+                                              <BarChart accessibilityLayer data={reportChartData}>
+                                                  <CartesianGrid vertical={false} />
+                                                  <XAxis
+                                                      dataKey="category"
+                                                      tickLine={false}
+                                                      tickMargin={10}
+                                                      axisLine={false}
+                                                      tickFormatter={(value) =>
+                                                          reportChartConfig[value as keyof typeof reportChartConfig]?.label
+                                                      }
+                                                  />
+                                                  <ChartTooltip
+                                                      cursor={false}
+                                                      content={<ChartTooltipContent hideLabel />}
+                                                  />
+                                                  <Bar
+                                                      dataKey="reports"
+                                                      strokeWidth={2}
+                                                      radius={8}
+                                                      activeIndex={2}
+                                                      activeBar={({ ...props }) => {
+                                                          return (
+                                                              <Rectangle
+                                                                  {...props}
+                                                                  fillOpacity={0.8}
+                                                                  stroke={props.payload.fill}
+                                                                  strokeDasharray={4}
+                                                                  strokeDashoffset={4}
+                                                              />
+                                                          )
+                                                      }}
+                                                  />
+                                              </BarChart>
+                                          </ChartContainer>
+                                      </CardContent>
+                                  </Card>
+                          </div>
+                      </div>)
+                  }
+                  <div className="z-30">
+                      <button
+                          onClick={toggleView}
+                          className="fixed bottom-8 right-8 px-4 py-2 rounded-full font-semibold bg-selected text-white shadow-lg hover:bg-primary-dark/90 focus:outline-none"
+                      >
+                          {isTableView ? 'Switch to Chart' : 'Switch to Table'}
+                      </button>
                   </div>
               </div>
           </TabsContent>
