92b37e1ad0bada158f3fd94bed98900784a8fa10
yousuf alansary <124580984+yousufalansary@users.noreply.github.com>
2024-11-26 08:12:36 +0300

    Web fe nav bar (#24)
    * added admin tab and clerk sigendin and out
    
    * integrated with clerk

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -2,10 +2,12 @@
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
+import { SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   // const { user, login, logout } = useAuthStore();
   const user = null;
+  const isAdmin = false;
 
   return (
     <nav className="sticky z-20 h-16 inset-x-0 top-0 w-full backdrop-blur-lg transition-all">
@@ -15,90 +17,99 @@ const Navbar = () => {
           EduLink
         </div>
         <div className="h-full flex items-center">
-          {!!user ? (
-            <>
+          <SignedIn>
+            <Link
+              href="/"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Home
+            </Link>
+            <Link
+              href="#features"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Chat
+            </Link>
+            <Link
+              href="#stats"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Schedule
+            </Link>
+            <Link
+              href="#start"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Material
+            </Link>
+            {isAdmin && (
               <Link
-                href="/"
+                href="/admin"
                 className={buttonVariants({
                   className: "font-semibold",
                   variant: "ghost",
                 })}
               >
-                Home
+                Admin
               </Link>
-              <Link
-                href="#features"
-                className={buttonVariants({
-                  className: "font-semibold",
-                  variant: "ghost",
-                })}
-              >
-                Chat
-              </Link>
-              <Link
-                href="#stats"
-                className={buttonVariants({
-                  className: "font-semibold",
-                  variant: "ghost",
-                })}
-              >
-                Schedule
-              </Link>
-              <Link
-                href="#start"
-                className={buttonVariants({
-                  className: "font-semibold",
-                  variant: "ghost",
-                })}
-              >
-                Material
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link
-                href="/"
-                className={buttonVariants({
-                  className: "font-semibold",
-                  variant: "ghost",
-                })}
-              >
-                Home
-              </Link>
-              <Link
-                href="#features"
-                className={buttonVariants({
-                  className: "font-semibold",
-                  variant: "ghost",
-                })}
-              >
-                Features
-              </Link>
-              <Link
-                href="#stats"
-                className={buttonVariants({
-                  className: "font-semibold",
-                  variant: "ghost",
-                })}
-              >
-                Statistics
-              </Link>
-              <Link
-                href="#start"
-                className={buttonVariants({
-                  className: "font-semibold",
-                  variant: "ghost",
-                })}
-              >
-                Start Learning
-              </Link>
-            </>
-          )}
+            )}
+          </SignedIn>
+
+          <SignedOut>
+            <Link
+              href="/"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Home
+            </Link>
+            <Link
+              href="#features"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Features
+            </Link>
+            <Link
+              href="#stats"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Statistics
+            </Link>
+            <Link
+              href="#start"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Start Learning
+            </Link>
+          </SignedOut>
         </div>
 
         <div className="h-full flex items-center">
-          {!!user ? (
-            <Button
+          <SignedIn>
+            {/* <Button
               className={buttonVariants({
                 className: "font-semibold",
                 variant: "ghost",
@@ -108,29 +119,40 @@ const Navbar = () => {
               }}
             >
               Logout
-            </Button>
-          ) : (
-            <>
-              <Link
-                href="/auth?tab=login"
+            </Button> */}
+
+            <SignOutButton>
+              <Button
                 className={buttonVariants({
                   className: "font-semibold",
                   variant: "ghost",
                 })}
               >
-                Login
-              </Link>
-              <Link
-                href="/auth?tab=register"
-                className={buttonVariants({
-                  className: "font-semibold bg-primary-light text-white",
-                  variant: "default",
-                })}
-              >
-                Sign Up
-              </Link>
-            </>
-          )}
+                Logout
+              </Button>
+            </SignOutButton>
+          </SignedIn>
+
+          <SignedOut>
+            <Link
+              href="/auth?tab=login"
+              className={buttonVariants({
+                className: "font-semibold",
+                variant: "ghost",
+              })}
+            >
+              Login
+            </Link>
+            <Link
+              href="/auth?tab=register"
+              className={buttonVariants({
+                className: "font-semibold bg-primary-light text-white",
+                variant: "default",
+              })}
+            >
+              Sign Up
+            </Link>
+          </SignedOut>
         </div>
       </div>
     </nav>
