0cfa636091b57252cd27790ea6167386c19d7466
AliAlsannaa <ans85122@gmail.com>
2024-12-10 12:44:32 +0300

    profile responsive

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -6,9 +6,9 @@ import { ProfileSection } from "@/components/ProfileSection";
 const ProfilePage = async () => {
   return (
     <MaxWidthWrapper className="my-8 px-4 mx-4">
-      <div className="flex flex-row gap-32">
+      <div className="flex flex-col md:flex-row gap-8 md:gap-32">
         <ProfileSection />
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 w-full md:w-auto">
           <CourseGridSchedule />
           <CourseGridTutoring />
         </div>
--- web/components/ProfileUpdateDialog.tsx
+++ web/components/ProfileUpdateDialog.tsx
@@ -26,19 +26,28 @@ export const ProfileUpdateDialog = () => {
           Update Profile
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <ProfileUpdateForm
-          onClose={() => {
-            setIsOpen(false);
-          }}
-        />
-      </DialogContent>
+<DialogContent 
+  className="
+    w-full 
+    max-w-full 
+    sm:max-w-[425px] 
+    p-4 sm:p-6 
+    rounded-lg
+  "
+>
+  <DialogHeader>
+    <DialogTitle className="text-xl sm:text-2xl font-semibold">Edit profile</DialogTitle>
+    <DialogDescription className="text-sm sm:text-base">
+      Make changes to your profile here. Click save when you're done.
+    </DialogDescription>
+  </DialogHeader>
+  <ProfileUpdateForm
+    onClose={() => {
+      setIsOpen(false);
+    }}
+  />
+</DialogContent>
+
     </Dialog>
   );
 };
